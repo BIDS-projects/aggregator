0559d74a071befc949c46682f9ca11c803e9861f
Alvin Wan <hi@alvinwan.com>
2016-02-14 01:35:20 -0800

    added  first test

--- tests/test_module_lda.py
+++ tests/test_module_lda.py
@@ -1,1 +1,28 @@
 import pytest
+
+import sys
+
+sys.path.append('bidsaggregator')
+
+from modules.lda import LDAModule
+from unittest.mock import MagicMock
+
+
+def test_load():
+    """test that loading from CSV works"""
+    filepath = 'tests/data/bids.csv'
+    filedata = 'data,keyword1,keyword2,keyword3,keyword4,keyword5,keyword6,keyword7,keyword8,keyword8,keyword9,keyword10'
+
+    f = open(filepath, 'w')
+    f.write(filedata)
+    f.close()
+
+    data = LDAModule().load(MagicMock(csv=filepath))
+    assert data['institution'] == 'bids'
+    assert data['topics'][0][0] == 'data'
+    assert data['topics'][0][1] == filedata.split(',')[1:]
+
+
+def test_parse():
+    """Tests that LDA parsed correctly"""
+    
